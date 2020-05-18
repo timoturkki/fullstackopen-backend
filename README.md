1e7bcdb23768983568e7abfba1d9d4b0041007d6
Timo Turkki <timo.turkki@frantic.com>
2020-05-18 14:49:27 +0300

    add phonebook endpoints

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Exercises for backend section of full stack open 2020 course
+
+## Commands
+
+- Start server `npm start`
+- Start server in development mode `npm run start`
+
+## Development
+
+One can use VSCode extension REST Client to make API calls, ready made requests in `requests`-folder.
---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3001;
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040040487",
+    id: 1,
+  },
+  {
+    name: "Teppo Winnipeg",
+    number: "040040480",
+    id: 2,
+  },
+  {
+    name: "Uolevi Sinisilmä",
+    number: "040666487",
+    id: 3,
+  },
+  {
+    name: "Marjatta Sinisilmä",
+    number: "040664487",
+    id: 4,
+  },
+];
+
+app.get("/", (_req, res) => {
+  res.send("<h1>Welcome to best phonebook in the world!</h1>");
+});
+
+app.get("/info", (_req, res) => {
+  const content = `
+    Phonebook has total ${persons.length} persons in it!<br><br>
+    ${new Date()}
+  `;
+  res.send(content);
+});
+
+app.get("/api/persons", (_req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  const { name, number } = body;
+  const nameExists = persons.some((person) => person.name === name);
+  const id = Math.floor(Math.random() * 9999999);
+  let error = "";
+
+  if (!name || !number) {
+    error = "Name and number are required";
+  } else if (nameExists) {
+    error = `Person with name ${name} is already saved`;
+  }
+
+  if (error) {
+    return res.status(400).json({ error });
+  }
+
+  const person = { name, number, id };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
